510f61303b6d423fe494a52ae80eb7dd35dbf4bd
Tace Flavius <tonialexe@yahoo.com>
2011-11-14 16:24:09 +0200

    Initial commit

---
+++ chat/admin.py
@@ -0,0 +1,11 @@
+from Chat.models import Chat
+from Chat.models import Message
+from Chat.models import Department
+from django.contrib import admin
+
+
+admin.site.register(Chat)
+admin.site.register(Message)
+admin.site.register(Department)
+
+
---
+++ chat/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Chat(models.Model):
+	start_date = models.DateTimeField(auto_now_add=True)
+	title = models.CharField(max_length=128)
+	owner = models.CharField(max_length=30)
+	end_date = models.DateTimeField()
+	department = models.ForeignKey('Department')
+	def __unicode__(self):
+		return self.title
+
+
+class Message(models.Model):
+	body = models.TextField()
+	date_sent = models.DateTimeField(auto_now=True)
+	date_edited = models.DateTimeField()
+	ip = models.IPAddressField()
+	chat = models.ForeignKey(Chat)
+	def __unicode__(self):
+		return self.chat.title
+
+class Department(models.Model):
+	name = models.CharField(max_length=128)
+	users = models.ManyToManyField(User)
+	def __unicode__(self):
+		return self.name
+	

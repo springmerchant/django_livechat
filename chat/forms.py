3d02d04fddfb064d0963e31cf783fcb6badf06c3
Tace Flavius <tonialexe@yahoo.com>
2011-12-09 19:11:43 +0200

    Created id's for each chat

--- chat/forms.py
+++ chat/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from chat.models import Message
 from chat.models import Chat
+from chat.models import Department
 
 class MessageForm(forms.ModelForm):
 	body = forms.CharField(label='Message',widget=forms.Textarea(attrs={'class':'message_body'}))
--- chat/views.py
+++ chat/views.py
@@ -6,92 +6,96 @@ from django.template import RequestContext
 from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
-from chat.forms import MessageForm,InformationForm
+from chat.forms import MessageForm, InformationForm
 from django.views.decorators.csrf import csrf_exempt
-from chat.models import Message,Chat,Visitor,Operator
+from chat.models import Message, Chat, Visitor, Operator, Department
 from django.shortcuts import redirect
 from django.utils import simplejson
 from django.contrib.auth.models import User
 import time
 
+@csrf_exempt
+def start_chat(request):
 
+    form = InformationForm()
+    
+    if request.POST:
+            chat = Chat()
+            form = InformationForm(request.POST)
+
+    if form.is_valid():
+            try:
+                operator = Operator.objects.get(id=1)
+            except Operator.DoesNotExist:
+                raise
+
+            try:
+                user = User.objects.get(id=1)
+            except User.DoesNotExist:
+                raise
+            try:
+                department = Department.objects.get(id=1)
+            except Department.DoesNotExist:
+                raise
+            visitor = Visitor(first_name=form.cleaned_data['first_name'])
+            visitor.save()
+            chat = Chat(operator=operator,department=department, status='3')
+            chat.save()
+	    return HttpResponseRedirect('/chat/view/%s/' % (chat.id))
+
+    return render_to_response('chat/start_chat.html', {"form": form})
 
 
 @csrf_exempt
-def start_chat(request):
-	
-	form=InformationForm()
-	
-	if request.POST:
-
-		chat = Chat()
-		form = InformationForm(request.POST)
-		
-		if form.is_valid():
-			try:
-				user = User.objects.get(id=1)
-			except User.DoesNotExist:
-				raise
-				
-			operator = Operator(first_name=form.cleaned_data['first_name'], user=user)
-			operator.save()
-			
-			visitor = Visitor(first_name=form.cleaned_data['first_name'])
-			visitor.save()
-						
-			chat = Chat(visitor=visitor, operator=operator, status='3')
-			chat.save()
- 
-			return HttpResponseRedirect('/chat/view/%s/' % (chat.id))
-
-	#	return HttpResponseRedirect('/thanks/') # Redirect after POST
-	return render_to_response('chat/start_chat.html',{"form":form})
-
-@csrf_exempt		
 def add_message(request, chat_id):
 	form = MessageForm
 	error = {}
-	
-	
-	chat = Chat.objects.get(id=chat_id)
-	
+
+	try:
+		chat = Chat.objects.get(id=chat_id)
+	except Chat.DoesNotExist:
+		raise
+
 	if request.method == "POST":
 		form = MessageForm(request.POST)
-	
+
 		if form.is_valid():
-			
 			f = form.save(commit=False)
 			f.ip = "1.2.3.4"
 			f.chat = chat
 			f.save()
-			
+
 			if request.is_ajax():
 				error['error'] = 'False'
 				return HttpResponse(simplejson.dumps(error))
 			else:
-				return redirect('/messages/%s/'% (f.chat.id))
+				return redirect('/messages/%s/' % (f.chat.id))
+
 
 def view_chat(request, chat_id):
 	form = MessageForm
-	chat = Chat.objects.get(id=chat_id)
+	try:
+		chat = Chat.objects.get(id=chat_id)
+	except    Chat.DoesNotExist:
+		raise
+	return render_to_response('chat/add_message.html', {"form": form, "chat": chat})
 
-	return render_to_response('chat/add_message.html',{"form":form,"chat":chat})	
 
 def view_messages(request, chat_id):
 	try:
 		messages = Message.objects.filter(chat__id=chat_id).order_by('date_sent')
 	except Message.DoesNotExist:
 		raise
-		
+
 	if request.is_ajax():
 		message_list = []
 		for message in messages:
-			message_dict ={'body':message.body,
-							'ip':message.ip,
-							'message_from':message.message_from,
-							'date_sent':message.date_sent.strftime("%I:%M %p"),
+			message_dict = {'body': message.body,
+							'ip': message.ip,
+							'message_from': message.message_from,
+							'date_sent': message.date_sent.strftime("%I:%M %p"),
 							}
 			message_list.append(message_dict)
 		return HttpResponse(simplejson.dumps(message_list))
-	
-	return render_to_response('chat/see_chat.html',{"messages":messages})
+
+	return render_to_response('chat/see_chat.html', {"messages": messages})
